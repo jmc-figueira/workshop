e4f41976f5e8014a85984ff88ff814a69d394071
João Figueira <joao@10-22-204-23.ed2.openfct.fct.unl.pt>
2014-02-10 11:02:31 +0000

    Added file

---
+++ hello.c
@@ -0,0 +1,14 @@
+//
+//  hello.c
+//  
+//
+//  Created by João Figueira on 10/02/14.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("Hello World, how are you today?\n");
+    return 0;
+}
